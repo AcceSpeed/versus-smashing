0e0223a926f6b18f7a69a18b4be9504ff888a060
AcceSpeed <v7a1.7.7@gmail.com>
2015-02-11 09:34:38 +0100

    TestNetwork
    MAJ avec prédiction et sync

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,22 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 	public float speed = 10f;
+	private float lastSynchronizationTime = 0f;
+	private float syncDelay = 0f;
+	private float syncTime = 0f;
+	private Vector3 syncStartPosition = Vector3.zero;
+	private Vector3 syncEndPosition = Vector3.zero;
 	
 	void Update()
 	{
 		if (networkView.isMine)
 		{
 			InputMovement();
+			InputColorChange();
+		}
+		else
+		{
+			SyncedMovement();
 		}
 	}
 	
@@ -31,15 +41,46 @@ public class Player : MonoBehaviour
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
 	{
 		Vector3 syncPosition = Vector3.zero;
+		Vector3 syncVelocity = Vector3.zero;
 		if (stream.isWriting)
 		{
 			syncPosition = rigidbody.position;
 			stream.Serialize(ref syncPosition);
+			
+			syncVelocity = rigidbody.velocity;
+			stream.Serialize(ref syncVelocity);
 		}
 		else
 		{
 			stream.Serialize(ref syncPosition);
-			rigidbody.position = syncPosition;
+			stream.Serialize(ref syncVelocity);
+			
+			syncTime = 0f;
+			syncDelay = Time.time - lastSynchronizationTime;
+			lastSynchronizationTime = Time.time;
+			
+			syncEndPosition = syncPosition + syncVelocity * syncDelay;
+			syncStartPosition = rigidbody.position;
 		}
+	}
+	
+	private void SyncedMovement()
+	{
+		syncTime += Time.deltaTime;
+		rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+	}
+
+	private void InputColorChange()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+			ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+	}
+	
+	[RPC] void ChangeColorTo(Vector3 color)
+	{
+		renderer.material.color = new Color(color.x, color.y, color.z, 1f);
+		
+		if (networkView.isMine)
+			networkView.RPC("ChangeColorTo", RPCMode.OthersBuffered, color);
 	}
 }
