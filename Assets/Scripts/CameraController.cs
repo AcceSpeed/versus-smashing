e196b4a15d416df1d2944aa87cdb84dd3d3116ad
Ardgevald <pombodias.miguel@gmail.com>
2015-02-20 10:06:07 +0100

    Giong on Main Controller (merge to master)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform tranPlayer;
+
+	private Transform tranCamera ;
+
+	void Start () {
+		tranCamera = this.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = new Vector3(
+			tranPlayer.position.x,
+			tranCamera.position.y,
+			tranCamera.position.z
+		);
+	}
+}
